a2e7dc34d297d66ca46efb518d02ad4a65a6bbde
MuriloDSantos <49132949+MuriloNSantos17@users.noreply.github.com>
2021-10-19 09:06:16 -0300

    Commit Inicial

---
+++ script.js
@@ -0,0 +1,11 @@
+function criaCalculadora()
+{
+    console.log('oi');
+}
+
+
+var calculadora = document.querySelector(".containerB");
+
+calculadora.addEventListener('click', function(e){
+    criaCalculadora();
+});
