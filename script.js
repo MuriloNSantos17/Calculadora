1cb0bceb26f8a1669b7a2e4583296b6bf2fa3461
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2021-10-19 21:19:46 -0300

    Commit correções

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
 function criaCalculadora()
 {
-    console.log('oi');
+    return {
+        display: document.querySelector('.display'),
+        
+        inicia(){
+            this.cliqueDosBotoes();
+            
+        },
+
+
+        clearDisplay(){
+            this.display.value="";
+        },
+    
+        cliqueDosBotoes() {
+            document.addEventListener('click', e =>{      
+                
+                const el = e.target;
+
+                if(el.classList.contains("btnNum"))
+                {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if(el.classList.contains('btnClear'))
+                {
+                    this.clearDisplay();
+                }
+            });
+
+           
+        },
+        
+       
+        btnParaDisplay(valor){
+            this.display.value += valor;
+        }
+    
+        
+    };
+    
+
+    
 }
 
 
-var calculadora = document.querySelector(".containerB");
 
-calculadora.addEventListener('click', function(e){
-    criaCalculadora();
-});
+var calculadora = criaCalculadora();
+calculadora.inicia();
