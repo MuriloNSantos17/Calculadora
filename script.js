f87a2c48f902ef54279ecb966e0692ae826353ef
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2021-11-01 21:54:59 -0300

    Commit

--- script.js
+++ script.js
@@ -1,50 +1,78 @@
 function criaCalculadora()
 {
     return {
+
         display: document.querySelector('.display'),
-        
-        inicia(){
-            this.cliqueDosBotoes();
+
+
+
+        inicia: function inicia(){
+            this.cliqueBotoes();
+            this.botaoEnter();
+        },
+
+        cliqueBotoes() {
             
-        },
+            document.addEventListener('click', (e)=>{
 
-
-        clearDisplay(){
-            this.display.value="";
-        },
-    
-        cliqueDosBotoes() {
-            document.addEventListener('click', e =>{      
-                
                 const el = e.target;
-
-                if(el.classList.contains("btnNum"))
+                
+                if(el.classList.contains('btnNum'))
                 {
                     this.btnParaDisplay(el.innerText);
                 }
-
                 if(el.classList.contains('btnClear'))
                 {
-                    this.clearDisplay();
+                    this.clear();
                 }
+                if(el.classList.contains('btnEqual'))
+                {
+                    this.calcular();
+                }
+
+                this.display.focus();
+
             });
 
-           
         },
-        
-       
-        btnParaDisplay(valor){
-            this.display.value += valor;
-        }
-    
-        
-    };
-    
 
-    
+        btnParaDisplay(valor)
+        {
+            this.display.value += valor;
+        },
+
+        clear(){
+            this.display.value='';
+        },
+
+        calcular(){
+            try {
+                const resultado = eval(this.display.value);
+
+                
+                this.clear();
+                this.btnParaDisplay(Number(resultado));
+            } 
+            catch (error) 
+            {
+                window.alert('Erro');
+                this.clear();
+            }
+        },
+
+        botaoEnter(){
+            this.display.addEventListener('keyup', (e)=>{
+                if(e.keyCode==13)
+                {
+                    this.calcular();
+                }
+            })
+        }
+
+    }
 }
 
 
+const calculadora = criaCalculadora();
 
-var calculadora = criaCalculadora();
 calculadora.inicia();
